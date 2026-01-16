778d5d498d91fa32fd848bffa7c4946178aeb619
roasmarialaura <roa20003@byui.edu>
2026-01-16 09:10:09 -0300

    I corrected Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -6,25 +6,24 @@ class Program
     {
         Console.Write("Please enter your grade percentage: ");
         string gradePercentage = Console.ReadLine();
-        int grade = int.Parse(gradePercentage);
+        int percent = int.Parse(gradePercentage);
 
         string letter = "";
-        string sign = "";
+        
 
-
-        if (grade >= 90)
+        if (percent >= 90)
         {
             letter = "A";
         }
-        else if (90 > grade && grade >= 80)
+        else if (percent >= 80)
         {
             letter = "B";
         }
-        else if (80 > grade && grade >= 70)
+        else if (percent >= 70)
         {
             letter = "C";
         }
-        else if (70 > grade && grade >= 60)
+        else if (percent >= 60)
         {
             letter = "D";
         }
@@ -33,23 +32,9 @@ class Program
             letter = "F";
         }
 
-        //Stretch Challenge
-        
-        int remainder = grade % 10;
+        Console.WriteLine($"Your grade is {letter}");
 
-
-        if (remainder >= 7 && grade >= 60)
-        {
-            sign = "+";
-        }
-        if (remainder < 3 && grade >= 60)
-        {
-            sign = "-";
-        }
-
-        Console.WriteLine($"Your grade is {letter}{sign}");
-
-        if (grade >= 70)
+        if (percent >= 70)
         {
             Console.WriteLine($"You passed the course. Congratulations!");
         }
@@ -58,5 +43,22 @@ class Program
             Console.WriteLine($"You didn't pass the course. Do your best the next time!");
         }
 
+        //Stretch Challenge
+
+        string sign = "";        
+        int lastDigit= percent % 10;
+
+        if (lastDigit >= 7 && percent >= 60)
+        {
+            sign = "+";
+        }
+        if (lastDigit < 3 && percent >= 60)
+        {
+            sign = "-";
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your grade is {letter}{sign}");
     }
+
 } 
