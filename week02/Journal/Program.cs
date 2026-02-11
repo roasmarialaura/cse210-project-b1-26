15ef1e512c48c834c80055aaa11509bd028ff4e6
roasmarialaura <roa20003@byui.edu>
2026-02-10 21:16:56 -0300

    Journal program done

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        Journal myJournal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+
+        string option ="";
+
+        while (option != "5")
+        {
+            Console.WriteLine("Please select one of the following choices: ");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("What would you like to do?");
+            option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    
+                    string randomPrompt = promptGenerator.GetRandomPrompt();
+                    Entry entry = new Entry();
+                    entry._date = DateTime.Now.ToShortDateString();
+                    entry._promptText = randomPrompt;
+                    Console.WriteLine($"{randomPrompt}");
+                    Console.WriteLine();
+                    entry._entryText = Console.ReadLine();
+                    myJournal.AddEntry(entry);
+                    break;
+                    
+                case "2":
+                    myJournal.DisplayAll();
+                    break;
+                case "3":
+                    Console.Write("What is the filename? ");
+                    string loadFile = Console.ReadLine();
+                    myJournal.LoadToFile(loadFile);
+                    break;
+                case "4":
+                    Console.Write("What is the filename? ");
+                    string saveFile = Console.ReadLine();
+                    myJournal.SaveToFile(saveFile);
+                    break;
+
+            }
+        }
+
     }
 }
