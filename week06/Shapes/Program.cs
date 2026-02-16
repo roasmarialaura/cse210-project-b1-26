0bb671afb07c015f19993f2c4613896aa47071f0
roasmarialaura <roa20003@byui.edu>
2026-02-16 18:15:58 -0300

    Shapes activity done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Blue", 5);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Red", 2, 4);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Yellow", 3);
+        shapes.Add(shape3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
