c29b4c0c69594aee15eebd814af9d0b02079bce3
roasmarialaura <roa20003@byui.edu>
2026-02-17 04:57:34 -0300

    Mindfulness activity done

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        //Console.WriteLine("Hello World! This is the Mindfulness Project.");
+       
+        
+
+        string option ="";
+
+        while (option != "4")
+        {
+            Console.WriteLine("Menu opcion ");
+            Console.WriteLine("1. Start breathing activity");
+            Console.WriteLine("2. Start reflecting activity");
+            Console.WriteLine("3. Start listing activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Select a choice from de menu ");
+            option = Console.ReadLine();
+
+            if (option == "1")
+            {
+                new BreathingActivity().Run();
+            }
+            else if (option == "2")
+            {
+                new ReflectingActivity().Run();
+            }
+            else if (option == "3")
+            {
+                new ListingActivity().Run();
+            }
+        }
+
+    
     }
 }
