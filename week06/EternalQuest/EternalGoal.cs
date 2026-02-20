7bef4cd0ef1214d61f114cc73fdc8d6368fc144f
roasmarialaura <roa20003@byui.edu>
2026-02-19 21:08:14 -0300

    EternalGoals class done

---
+++ week06/EternalQuest/EternalGoal.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class EternalGoal : Goal
+{
+    public EternalGoal(string name, string description, int points) : base (name, description, points)
+    {
+        
+    }
+    
+    
+    public override void RecordEvent()
+    {
+        
+    }
+    
+    public override bool IsComplete()=> false;
+
+    public override string GetStringRepresentation()
+    {
+        return $"EternalGoal|{GetShortName()}|{GetDescription()}|{GetPoints()}";
+    }
+}
