90bc88e80c3ac8de39df606bb6e83796ab47fedc
roasmarialaura <roa20003@byui.edu>
2026-02-19 21:11:37 -0300

    EternalQuest program done

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Diagnostics.Tracing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        //Console.WriteLine("Hello World! This is the EternalQuest Project.");
+
+        GoalManager manager = new GoalManager();
+        manager.Start();
+
     }
+    
+    
 }
+    
